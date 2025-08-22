65a28667a796ff6eae06d36193b8f2abc10e294d
Andy Gayton <andy@thecablelounge.com>
2025-08-21 20:34:49 -0400

    .

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -177,7 +177,6 @@ async fn stream_existing_events(app: AppHandle, store: Store) -> Result<()> {
     Ok(())
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
